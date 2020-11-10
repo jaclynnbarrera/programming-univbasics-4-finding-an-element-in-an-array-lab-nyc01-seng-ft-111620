939af8802874e1864a8b5801d28c95a3a1b67f14
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-10 15:30:40 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,23 @@
-
 def find_element_index(array, value_to_find)
 
-array.index(value_to_find)
+count = 0 
+  while count < array.length do 
+  
+  count += 1
+  
+  end
+
 
 end
+
+
+
+
+
+
+
+#def find_element_index(array, value_to_find)
+
+#array.index(value_to_find)
+
+#end
