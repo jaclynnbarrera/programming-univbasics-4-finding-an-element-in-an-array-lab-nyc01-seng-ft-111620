25616c25d8f832c86bbaadc85f9d51ec49d8dd44
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-10 15:13:06 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,12 @@
 
 def find_element_index(array, value_to_find)
 
+array.push("value_to_find")
+
+array.index("value_to_find")
+
+
+
 counter = 0
 while counter < array.length do
   array[counter] == "value_to_find"
